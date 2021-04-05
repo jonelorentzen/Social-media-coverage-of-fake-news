0925a8c8ba20dd3aeb55b96a5157da2a01288270
Ramtin Hafezinejad <Ramtin.Hafezinejad@gmail.com>
2021-04-05 16:51:14 +0200

    sign in with twitter not done

---
+++ NewvueApp/forgerynewstrackervue/src/backend_api/twitter.js
@@ -0,0 +1,24 @@
+// import Twitter from 'twitter-lite';
+// // import axios from 'axios';
+
+
+// export default class TwitterLogin{
+//   Login(){
+//     const client = new Twitter({
+//       version: "2",
+//       consumer_key: "zpt0wHS9rEJOMs5kSnnktHIOh",
+//       consumer_secret: "14g8CorLV7N5TFVZ0Ot7iFrCq4foAYF9emMCI1o4QNUANdiXDs"
+//     });
+    
+//     client
+      
+//       .getRequestToken("http://127.0.0.1:8080")
+//       .then(res =>
+//         console.log({
+//           reqTkn: res.oauth_token,
+//           reqTknSecret: res.oauth_token_secret
+//         })
+//       )
+//       .catch(console.error);
+//   }
+//   }
