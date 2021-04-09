76cf61697871820a402d0b79cc4d0ff2c4d8cf20
Ramtin Hafezinejad <Ramtin.Hafezinejad@gmail.com>
2021-04-09 17:17:35 +0200

    Changed view

--- NewvueApp/forgerynewstrackervue/src/views/About.vue
+++ NewvueApp/forgerynewstrackervue/src/views/About.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="about">
     <h1>About Us!</h1>
-    <p>We are a group of 3 students who are studying computer science at UiS.</p>
-    <p>This website is our bachelor thesis</p>
+    
 
   </div>
 </template>
